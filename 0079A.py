c2c8e7ecc71c96046b00d5348b51686b11176207
Alfred Huang <alfred.h@163.com>
2013-01-22 14:05:17 +0800

    0079A, 0027A

--- 0079A.py
+++ 0079A.py
@@ -2,15 +2,19 @@ from sys import stdin
 
 (x, y) = [int(buf) for buf in stdin.readline().strip().split()]
 
+k = min(x//2, y//24)
+
+x -= 2 * k
+y -= 24 * k
 
 ans = 0
 while y >= 2 and x*100 + y*10 >= 220:
     ans = 1 - ans
     if ans == 1:
-        y -= 22 - max(x, 2) * 10
-        x -= max(x, 2)
+        y -= 22 - min(x, 2) * 10
+        x -= min(x, 2)
     else:
-        x -= 2 - max((y-2)//10, 2)
-        y -= 2 + max((y-2)//10, 2) * 10
+        x -= 2 - min((y-2)//10, 2)
+        y -= 2 + min((y-2)//10, 2) * 10
 
 print('Ciel' if ans == 1 else 'Hanako')
