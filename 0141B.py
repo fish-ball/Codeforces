4cc0008ad907e5d72e26f22104b07f47088fdfdd
Alfred Huang <alfred.h@163.com>
2013-02-07 01:19:49 +0800

    0228B.py, 0141B.py

--- 0141B.py
+++ 0141B.py
@@ -1,36 +1,23 @@
 from sys import stdin
 
-(na, ma) = [int(buf) for buf in stdin.readline().strip().split()]
-a = [int(stdin.readline().strip()[::-1], 2) for i in range(0, na)]
+(a, x, y) = [int(x) for x in stdin.readline().strip().split()]
 
-(nb, mb) = [int(buf) for buf in stdin.readline().strip().split()]
-b = [int(stdin.readline().strip()[::-1], 2) for i in range(0, nb)]
-
-def count1(x):
-    x = (x & 0x55555555555555555555555555555555) + ((x>>0x01) & 0x55555555555555555555555555555555)
-    x = (x & 0x33333333333333333333333333333333) + ((x>>0x02) & 0x33333333333333333333333333333333)
-    x = (x & 0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F) + ((x>>0x04) & 0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F)
-    x = (x & 0x00FF00FF00FF00FF00FF00FF00FF00FF) + ((x>>0x08) & 0x00FF00FF00FF00FF00FF00FF00FF00FF)
-    x = (x & 0x0000FFFF0000FFFF0000FFFF0000FFFF) + ((x>>0x10) & 0x0000FFFF0000FFFF0000FFFF0000FFFF)
-    x = (x & 0x00000000FFFFFFFF00000000FFFFFFFF) + ((x>>0x20) & 0x00000000FFFFFFFF00000000FFFFFFFF)
-    return x
-
-def getval(x, y):
-    cnt = 0
-    for i in range(0, na):
-        if i+x in range(0, nb):
-            if y > 0:
-                cnt += count1((a[i]<<y) & b[i+x])
-            else:
-                cnt += count1(a[i] & (b[i+x]<<-y))
-    return cnt
-
-(ans, xx, yy) = (0, 0, 0)
-for x in range(1-na, nb):
-    for y in range(1-ma, mb):
-        val = getval(x, y)
-        if ans  < val:
-            (ans, xx, yy) = (val, x, y)
-
-print('%d %d' % (xx, yy))
-                
+if y % a == 0:
+    print(-1)
+elif y < a:
+    if -a < x+x < a:
+        print(1)
+    else:
+        print(-1)
+elif y // a % 2 == 1:
+    if -a < x+x < a:
+        print((y//a-1)//2*3+2)
+    else:
+        print(-1)
+else:
+    if -a < x < 0:
+        print(y//a//2*3)
+    elif 0 < x < a:
+        print(y//a//2*3+1)
+    else:
+        print(-1)
