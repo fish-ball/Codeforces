d86b5266b319d0b5413e1a39b9f977fe7077667a
Alfred Huang <alfred.h@163.com>
2013-02-06 15:30:48 +0800

    Update 0266B.py

--- 0266B.py
+++ 0266B.py
@@ -5,8 +5,12 @@ from sys import stdin, stdout
 s = list(stdin.readline().strip())
 
 for i in range(0, t):
+    y = True
     for j in range(1, n):
-        if s[-j] == 'G' and s[-1-j] == 'B':
+        if y and s[-j] == 'G' and s[-1-j] == 'B':
             (s[-j-1], s[-j]) = (s[-j], s[-1-j])
+            y = False
+        elif y == False:
+            y = True
 
 print(''.join(s))
